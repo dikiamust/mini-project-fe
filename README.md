a576c0d9b05d373a7d25910adf8760fcba0a9c65
dikiamust <agungmustaqimdiki@gmail.com>
2024-09-12 19:29:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pokemon List
+
+This web application showcases a dynamic list of Pokémon, seamlessly integrated with the PokeAPI, written in [Next.js](https://nextjs.org/).
+
+## Install dependencies
+
+```bash
+yarn install
+```
+
+## Running the application
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Pokemon List</title>
+        <meta name="description" content="A list of Pokemon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Pokemon List</h1>
+      </main>
+    </>
+  );
+}
